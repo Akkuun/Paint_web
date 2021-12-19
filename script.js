7e61d60575448706e669a6248ccb0588dcbce5de
Mathis <mathis.duban@etu.umontpellier.fr>
2021-12-20 00:08:36 +0100

    ajout carre fonction + bug double ajout à rectifier

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-
-
 $(document).ready(function () {
     var buttonsArray = [false, false, false, false, false, false, false, false, false];
     var mousePressed = false;
     var backRGB = document.getElementById("colors").value;
-    var backRGBCopie = document.getElementById("colors").value;
-    var isEnable = false;
+    var isCarre = false
+
 
 //tracage sur toutes les div des cases du tableaux
-    $(".case").mouseover(function () {
+    $
+    (".case").mouseover(function () {
 
         if (mousePressed === true) {
             $(this).css("background-color", backRGB);
@@ -19,7 +18,7 @@ $(document).ready(function () {
 //fonction gomme, cas 1 on appuie une fois et on gomme les pixels que l'on souhaite
     $("#gomme").click(function () {
 
-            backRGB="#808080";
+        backRGB = "#808080";
 
 
     });
@@ -50,4 +49,23 @@ $(document).ready(function () {
     document.oncontextmenu = function () {
         return false;
     }
+
+    $("#circle").click(function () {
+        isCarre = !isCarre
+        if (isCarre===true) {
+            $('.case').click(function (){
+                d3.select(this).append("svg").attr("width", 50).attr("height", 50).append("circle").attr("cx", 10).attr("cy", 10).attr("r", 10).style("fill", backRGB);
+            })
+        }
+        isCarre=false
+    });
+    $("#square").click(function () {
+        isCarre = !isCarre
+        if (isCarre) {
+            $('.case').click(function (){
+                d3.select(this).append("svg").attr("width", 50).attr("height", 50).append("rect").attr("cx", 10).attr("cy", 10).attr("r", 10).style("fill", backRGB);
+            })
+        }
+    });
+
 });
